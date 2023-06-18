95846caae63c86dddb90dfb72effcb04efa10255
wutchara <sstmuham@gmail.com>
2023-06-18 16:28:09 +0700

    Add initialize file

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+
+};
+
+
+ex1 = ["h", "e", "l", "l", "o"];
+console.log('EX1:', reverseString(ex1));
+
+ex2 = ["H", "a", "n", "n", "a", "h"];
+console.log('EX2:', reverseString(ex1));
