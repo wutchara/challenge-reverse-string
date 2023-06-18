8d779a348fea0a143be385b9bbc8b12f16dfda9c
wutchara <sstmuham@gmail.com>
2023-06-18 16:50:32 +0700

    Update solution

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
+// Runtime => 88 ms, Memory => 49 MB
 var reverseString = function (s) {
-
+    s = s.reverse();
 };
+
+// Runtime => 96 ms, Memory => 49.6 MB
+// var reverseString = function (s) {
+//     lastIndex = s.length-1;
+//     firstIndex = 0;
+//     for (let i = lastIndex; i > firstIndex; i--) {
+//         tmp = s[i];
+//         s[i] = s[firstIndex];
+//         s[firstIndex] = tmp;
+//         firstIndex++;
+//     }
+// };
 
 
 ex1 = ["h", "e", "l", "l", "o"];
