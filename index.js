6857e13c6c0de6d5ef8e6061543f310d04f1a295
wutchara <sstmuham@gmail.com>
2023-06-18 17:01:27 +0700

    Update solution

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ var reverseString = function (s) {
 //     }
 // };
 
+// Runtime => 107 ms, Memory => 53.9 MB
+// var reverseString = function (s, start = 0, end) {
+//     if (start > end) {
+//         return
+//     }
+
+//     if (!end) {
+//         // first round
+//         end = s.length - 1;
+//     }
+
+//     tmp = s[start];
+//     s[start] = s[end];
+//     s[end] = tmp;
+//     reverseString(s, start + 1, end - 1);
+// };
+
 
 ex1 = ["h", "e", "l", "l", "o"];
 reverseString(ex1);
@@ -26,4 +43,4 @@ console.log('EX1:', ex1);
 
 ex2 = ["H", "a", "n", "n", "a", "h"];
 reverseString(ex2);
-console.log('EX2:',ex2 );
+console.log('EX2:', ex2);
